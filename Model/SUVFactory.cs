ae67d59138eaf961cb078197f99d498e1683be8c
Aurionpro <Aurionpro@ACER1208>
2023-09-22 19:59:33 +0530

    Add project files.

---
+++ Model/SUVFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryLibrary.Model
+{
+    public class SUVFactory:IAutoFactory
+    {
+        public ITesla CreateTesla()
+        {
+            return new TeslaSUV();
+        }
+
+        public IBMW CreateBMW()
+        {
+            return new BMWSUV();
+        }
+
+        public IAudi CreateAudi()
+        {
+            return new AudiSUV();
+        }
+    }
+}
